2bb58df0e588510cbcff6abec1e448eeb1609842
IvanSlivenko <ivan8822@ukr.net>
2025-08-23 12:17:38 +0300

    23-08-2025-12-17

--- project/app/page.js
+++ project/app/page.js
@@ -1,7 +1,30 @@
 import Image from "next/image";
+import Link from "next/link";
+import { bebas, geistSans, ibmPlex } from "./fonts/fonts";
 
-export default function Home() {
+
+async function fetchData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const result = await  res.json();
+  return result;
+}
+
+export default async function Home() {
+  const posts = await fetchData();
+
   return (
-    <h1>Головна сторінка</h1>
+    <div>
+
+      {/* <h1>Головна сторінка</h1> */}
+      {posts.map(el=>(
+        <div key={el.id} className="post">
+          <h2>{el.title}</h2>
+          <p className={`${ibmPlex.className} myCustomClass`}>{el.body}</p>
+          <Link href={`/post/` + el.id}>Детальніше...</Link>
+        </div>
+      ))}
+
+      
+    </div>
   );
 }
